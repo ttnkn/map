7b40d845726877d0dd64f419d3aa5766e62c1b4c
nerdyscout <nerdyscout@posteo.de>
2019-12-15 12:19:31 +0000

    upgrading ongoing

---
+++ pax/index.js
@@ -0,0 +1,65 @@
+var IconStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+//        anchor: [0.5, 0.5],
+//        anchorXUnits: 'fraction',
+//        anchorYUnits: 'pixels',
+        color: '#8959A8',
+        src: '../img/bike.png',
+        crossOrigin: 'anonymous'
+    })
+});
+
+var IconFeature = new ol.Feature({
+    style: IconStyle
+});
+
+var VectorSource =  new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: 'https://api.opensensemap.org/boxes?bbox=9.118815,47.653129,9.228427,47.698786&format=geojson&exposure=mobile',
+    feature: IconFeature
+});
+
+var VectorTile = new ol.source.XYZ({
+    url: 'http://tile.memomaps.de/tilegen/{z}/{x}/{y}.png ',
+    attributions: 'Tiles &#169; <a href="memomaps.de">MeMoMaps</a> | Map &#169; <a href="https://www.openstreetmap.org">OSM</a>'
+});
+
+var map = new ol.Map({
+    target: document.getElementById('map'),
+    layers: [
+        new ol.layer.Tile({
+            source: VectorTile
+        }),
+        new ol.layer.Vector({
+            source: VectorSource,
+        })
+    ],
+    controls: ol.control.defaults({ zoom: true, attribution: true }),
+    view: new ol.View({
+        center: ol.proj.fromLonLat([9.173, 47.672]),
+        zoom: 15,
+//        MaxZoom: 14,
+//        MinZoom: 10
+    })
+});
+
+/*
+var url = 'https://api.opensensemap.org/statistics/idw?bbox=9.118815,47.653129,9.228427,47.698786&phenomenon=Temperatur&gridType=hex&cellWidth=0.2';
+fetch(url).then(value => {
+    value.json().then(value => {
+        var featureJson = value.data.featureCollection;
+        var features = (new ol.format.GeoJSON()).readFeatures(featureJson);
+
+        var vectorSource = new ol.source.Vector({
+            features: features,
+            projection: ol.proj.get('EPSG:4326')
+        });
+
+        var vectorLayer = new ol.layer.Vector({
+            source: vectorSource,
+        });
+
+        map.addLayer(vectorLayer);
+    });
+}, error => { console.log(error) });
+*/
