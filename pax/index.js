e4fcc202876294c917a50d3b478993cd6d4de4b6
nerdyscout <nerdyscout@posteo.de>
2019-12-15 15:42:08 +0000

    GeoStyleFunc

--- pax/index.js
+++ pax/index.js
@@ -1,27 +1,38 @@
-var IconStyle = new ol.style.Style({
-    image: new ol.style.Icon({
-//        anchor: [0.5, 0.5],
-//        anchorXUnits: 'fraction',
-//        anchorYUnits: 'pixels',
-        color: '#8959A8',
-        src: '../img/bike.png',
-        crossOrigin: 'anonymous'
+var GeoStyle = {
+    'Point': new ol.style.Style({
+        image: new ol.style.Icon({
+//            anchor: [0.5, 0.5],
+//            anchorXUnits: 'fraction',
+//            anchorYUnits: 'pixels',
+            src: '../img/bike.png',
+//            scale: 0.5,
+            crossOrigin: 'anonymous'
+        })
+    }),
+    'Circle': new ol.style.Circle({
+        fill: new ol.style.Fill({
+            color: 'rgba(255,255,255,0.4)'
+        }),
+        stroke: ol.style.Stroke({
+            color: '#3399CC',
+            width: 1.25
+        }),
+        radius: 5
     })
-});
+};
 
-var IconFeature = new ol.Feature({
-    style: IconStyle
-});
+function GeoStyleFunc (feature,resolution) {
+    return GeoStyle[feature.getGeometry().getType()];
+}
 
 var VectorSource =  new ol.source.Vector({
     format: new ol.format.GeoJSON(),
     url: 'https://api.opensensemap.org/boxes?bbox=9.118815,47.653129,9.228427,47.698786&format=geojson&exposure=mobile',
-    feature: IconFeature
 });
 
 var VectorTile = new ol.source.XYZ({
     url: 'http://tile.memomaps.de/tilegen/{z}/{x}/{y}.png ',
-    attributions: 'Tiles &#169; <a href="memomaps.de">MeMoMaps</a> | Map &#169; <a href="https://www.openstreetmap.org">OSM</a>'
+    attributions: 'Map &#169; <a href="https://www.openstreetmap.org">OSM</a> | Tiles &#169; <a href="memomaps.de">MeMoMaps</a> | Data &#169; <a href="https://opensensemap.org/">OpenSenseMap</a>'
 });
 
 var map = new ol.Map({
@@ -32,6 +43,7 @@ var map = new ol.Map({
         }),
         new ol.layer.Vector({
             source: VectorSource,
+            style: GeoStyleFunc
         })
     ],
     controls: ol.control.defaults({ zoom: true, attribution: true }),
